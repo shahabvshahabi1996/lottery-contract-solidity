4c6d104d5f0955e72ac99fbf4243ea6fab11ae08
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-10 04:39:34 +0330

    added test methods and libraries

---
+++ test/Lottery.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const {interface , bytecode} = require('../compile');
+var provider = ganache.provider();
+
+const web3 = new Web3(provider);
+
+let accounts;
+let lottery;
+
+beforeEach(async ()=>{
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    // use one of those accounts to deploy 
+    //the Contract
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({ data : bytecode })
+     .send({from : accounts[0] , gas : '1000000'})
+    lottery.setProvider(provider);    
+});
