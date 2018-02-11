ddc17ea49e54dd97c948de30028410e98ae44e4a
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-11 16:06:28 +0330

    deploying on rinkeby test netwokr

--- deploy.js
+++ deploy.js
@@ -17,6 +17,7 @@ const deploy = async () => {
      .deploy({ data : bytecode })
      .send({ from : accounts[0] , gas : '1000000'});
 
+     console.log(interface);
      console.log('Contract Deployed to ',result.options.address);
      result.setProvider(provider);
 }
